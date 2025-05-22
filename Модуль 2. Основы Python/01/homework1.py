264f2569b3ba7add922f6ac8b121ecb6b07cfa81
Alexandr Meshchriakov <avm@sh-inc.ru>
2025-05-22 20:41:08 +0200

    add function to calculate string length with example usage

--- Модуль 2. Основы Python/01/homework1.py
+++ Модуль 2. Основы Python/01/homework1.py
@@ -2,3 +2,19 @@
 # Пример:
 # Ввод: "Hello, World!"
 # Вывод: 13
+
+
+def string_length(s):
+    """
+    Функция принимает строку и возвращает ее длину.
+
+    :param s: Входная строка
+    :return: Длина строки
+    """
+    return len(s)
+
+# Пример использования функции
+if __name__ == "__main__":
+    input_string = "Hello, World!"
+    length = string_length(input_string)
+    print(f"Длина строки '{input_string}' составляет {length} символов.")
